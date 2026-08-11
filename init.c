773686018343f89f581218bc97747c0cdaefbf33
douaeabrouri <abrouridoae@gmail.com>
2026-08-11 22:11:10 +0100

    .

--- init.c
+++ init.c
@@ -2,33 +2,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
-void *routine(void *arg){
-    
+void *routine(void *arg)
+{
+
     t_coder *coder;
 
     coder = (t_coder *)arg;
-    // printf("coder %d start\n", coder->id);
     // while(...){
+    pthread_mutex_lock(&coder->left->mutex);
+    pthread_mutex_lock(&coder->right->mutex);
+    printf("coder id %d, left dongle %d right dongle %d\n", coder->id, coder->left->id, coder->right->id);
     //     compile();
+    sleep(2);
+    pthread_mutex_unlock(&coder->left->mutex);
+    pthread_mutex_unlock(&coder->right->mutex);
     //     debug();
+
     //     refactor();
     // }
     return (NULL);
 }
 
-
-void init_dongles(t_dongle *dongles, int num_dongles) {
+void init_dongles(t_dongle *dongles, int num_dongles)
+{
     int i = 0;
-    while (i < num_dongles) {
+    while (i < num_dongles)
+    {
         dongles[i].id = i + 1;
+        pthread_mutex_init(&dongles[i].mutex, NULL);
         i++;
     }
 }
 
 pthread_mutex_t mutex;
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
     t_data *data;
     t_coder *coders;
     // int mutex = 0;
@@ -41,19 +52,18 @@ int main(int argc, char **argv){
     pthread_t thread[data->number_of_coders];
     init_dongles(data->dongles, data->number_of_coders);
 
-    while (i < data->number_of_coders){
+    while (i < data->number_of_coders)
+    {
         int id = i + 1;
         coders[i].id = id;
         coders[i].data = data;
 
         coders[i].left = &data->dongles[i];
         coders[i].right = &data->dongles[id % data->number_of_coders];
-        
+
         pthread_create(&thread[i], NULL, routine, &coders[i]);
         pthread_join(thread[i], NULL);
-        // int n = pthread_mutex_init(&mutex, NULL);
-        printf("mutex %d\n", n);
-        printf("coder id %d left %d right %d\n", coders[i].id, coders[i].left->id, coders[i].right->id);
+        int n = pthread_mutex_init(&mutex, NULL);
         i++;
     }
 
