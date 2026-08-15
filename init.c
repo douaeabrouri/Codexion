c1f026f530af8d6204c2f945d63342e9f105e19e
mohamed amine amazzal <amazzalhusa@gmail.com>
2026-08-15 22:12:24 +0100

    .

--- init.c
+++ init.c
@@ -1,6 +1,6 @@
 #include "codexion.h"
 
-void init_dongles(t_data *data, t_dongle *dongles, int num_dongles){
+void init_dongles(t_dongle *dongles, int num_dongles){
     int i = 0;
     while (i < num_dongles)
     {
@@ -12,38 +12,39 @@ void init_dongles(t_data *data, t_dongle *dongles, int num_dongles){
 
 void compile(t_coder *coder){
 
-    long start = get_time_ms();
+    coder->last_compile = get_time_ms();
+    printf("coder id %d last comipile %ld\n", coder->id, coder->last_compile);
+    // printf("%ld Coder %d is compiling\n", get_time_ms() - coder->data->start_time, coder->id);
     ft_usleep(coder->data->time_to_compile);
     coder->finish_compile++;
-    printf("%ld Coder %d is compiling\n", get_time_ms() - coder->data->start_time,coder->id);
 }
 
 void debug(t_coder *coder){
-    printf("%ld Coder %d is debugging\n", get_time_ms() - coder->data->start_time,coder->id);
+    // printf("%ld Coder %d is debugging\n", get_time_ms() - coder->data->start_time,coder->id);
     ft_usleep(coder->data->time_to_debug);
 }
 
 void refactor(t_coder *coder){
-    printf("%ld Coder %d is refactoring\n", get_time_ms() - coder->data->start_time,coder->id);
+    // printf("%ld Coder %d is refactoring\n", get_time_ms() - coder->data->start_time,coder->id);
     ft_usleep(coder->data->time_to_refactor);
 }
-
+int counter = 0;
 void *routine(void *arg)
 {
     t_coder *coder;
-
+    counter++;
+    printf("counter %d\n", counter);
     coder = (t_coder *)arg;
-    // while(start < coder->data->number_of_coders){
+    // while(coder->data->simulation_end < 0){
     pthread_mutex_lock(&coder->left->mutex);
     pthread_mutex_lock(&coder->right->mutex);
     compile(coder);
     // if (current - coder->last_compile > coder->data->time_to_burnout)
-    //     printf("the coder shoul0.d stop!");
+    //     printf("the coder shoul0,d stop!");
     pthread_mutex_unlock(&coder->left->mutex);
     pthread_mutex_unlock(&coder->right->mutex);
     debug(coder);
     refactor(coder);
-    // start++;
     // }
     return (NULL);
 }
@@ -52,7 +53,6 @@ void *routine(void *arg)
 int main(int argc, char **argv){
     t_data *data;
     t_coder *coders;
-    // int mutex = 0;
     int i = 0;
 
     data = parsing(argv, argc);
@@ -61,7 +61,7 @@ int main(int argc, char **argv){
     coders = malloc(sizeof(t_coder) * data->number_of_coders);
 
     pthread_t thread[data->number_of_coders];
-    init_dongles(data, data->dongles, data->number_of_coders);
+    init_dongles(data->dongles, data->number_of_coders);
 
     while (i < data->number_of_coders)
     {
